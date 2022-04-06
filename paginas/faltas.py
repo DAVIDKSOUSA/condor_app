f11645848f79ae177fcbc799dba0b447fd4d6f56
David Sousa <DAVID_AFA84@HOTMAIL.COM>
2022-04-06 20:25:32 -0300

    - Figura página qts - Layout avisos

--- paginas/faltas.py
+++ paginas/faltas.py
@@ -121,6 +121,75 @@ def faltas():
         st.write(f"Abrir planilha no [Google Sheet]({GSHEET_URL})")
         st.dataframe(get_data(gsheet_connector))
 
+        #filgueiras
 
+    def get_data(gsheet_connector) -> pd.DataFrame:
+        values = (
+            gsheet_connector.values()
+                .get(
+                spreadsheetId=SPREADSHEET_ID,
+                range=f"{SHEET_NAME}!E:H",
+            )
+                .execute()
+        )
 
+        df = pd.DataFrame(values["values"])
+        df.columns = df.iloc[0]
+        df = df[1:]
+        return df
 
+    def add_row_to_gsheet(gsheet_connector, row) -> None:
+        gsheet_connector.values().append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!E:H",
+            body=dict(values=row),
+            valueInputOption="USER_ENTERED",
+        ).execute()
+
+        # st.set_page_config(page_title="Bug report", page_icon="🐞", layout="centered")
+
+    st.title("RETIRADA DE FALTAS")
+
+    gsheet_connector = connect_to_gsheet()
+
+    # texto sidebar
+    st.sidebar.write(
+        f"Esta página é permitida somente para o efetivo da Seção de Apoio do 1/2GT, somente usuários com e-mail \n"
+        f"cadastrados podem modificar a [Planilha de Retirada de Faltas]({GSHEET_URL})."
+    )
+    st.sidebar.write(
+        "Caso haja demandas procure algum militar do Apoio.\n"
+
+    )
+    st.sidebar.write(
+        "Att, Ten David"
+    )
+
+    form = st.form(key="teste")
+
+    with form:
+        st.title('Ten David')
+        # cols = st.columns((1, 1))
+        # author = cols[0].text_input("Ten David")
+        cols = st.columns(2)
+        motivo = cols[0].radio(
+            "Motivo:", ["Presente", "Voo", "Sobreaviso", "Dispensado", "Férias", "Outro"], index=0
+        )
+
+        date = cols[1].date_input("Data")
+        time = cols[1].time_input("Horário")
+        comment = st.text_area("Comentários")
+        submitted = st.form_submit_button(label="Registrar")
+
+    if submitted:
+        add_row_to_gsheet(
+            gsheet_connector,
+            [[str(motivo), str(comment), str(date), str(time)]],
+        )
+        st.success("Registro realizado.")
+        st.balloons()
+
+    expander = st.expander("Registros")
+    with expander:
+        st.write(f"Abrir planilha no [Google Sheet]({GSHEET_URL})")
+        st.dataframe(get_data(gsheet_connector))
