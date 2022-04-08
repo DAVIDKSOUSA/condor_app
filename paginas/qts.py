85af156db0fa894a97bc44adaf17917b96ae3a71
David Sousa <DAVID_AFA84@HOTMAIL.COM>
2022-04-08 19:14:59 -0300

    Atualizações Gerais

--- paginas/qts.py
+++ paginas/qts.py
@@ -6,12 +6,13 @@ from PIL import Image
 def qts():
     #st.write('___')
     #st.image(load_image_from_local("asset/images/chef-transformer-transparent.png"), width=300)
-    # image = Image.open('qts/qts.png')
+    image = Image.open('qts/8-14.jpg')
+    st.image(image)
     st.markdown("<h3 style='text-align: center; color:#003366; font-size:40px;'><b>QTS de 11 a 15 de abril de 2022<b></h3>",
                 unsafe_allow_html=True)
-    st.image('/Users/davidsousa/condor_app/qts/8-14.jpg', use_column_width='always')
+    #st.image('/Users/davidsousa/condor_app/qts/8-14.jpg', use_column_width='always')
     st.write('___')
     st.markdown("<h3 style='text-align: center; color:#003366; font-size:40px;'><b>QTS de 11 a 15 de abril de 2022<b></h3>",
                 unsafe_allow_html=True)
-    st.image('/Users/davidsousa/condor_app/qts/8-14.jpg', use_column_width='always')
+    #st.image('/Users/davidsousa/condor_app/qts/8-14.jpg', use_column_width='always')
 
